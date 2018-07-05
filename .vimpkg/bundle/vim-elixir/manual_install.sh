6d52f5c547a07fa5786aff5444e6d6713670b459
appositum <appositum@protonmail.com>
2018-07-04 22:58:47 -0300

    Remove vim folders

--- .vimpkg/bundle/vim-elixir/manual_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -R autoload/ ~/.vim/autoload/ &&
-    cp -R compiler/ ~/.vim/compiler/ &&
-    cp -R ftdetect/ ~/.vim/ftdetect/ &&
-    cp -R ftplugin/ ~/.vim/ftplugin/ &&
-    cp -R indent/ ~/.vim/indent/ &&
-    cp -R syntax/ ~/.vim/syntax/
