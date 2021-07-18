1a519a2e1c708667f291baaeb9593f4d3a202c69
appositum <appositum@protonmail.com>
2021-07-18 05:25:48 -0300

    Remove geometry from full screenshot script

--- .config/sway/scripts/screenshot_full.sh
+++ .config/sway/scripts/screenshot_full.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 filename="$(date "+%F_%H-%M-%S")"
-grim -t png -g '1920,0 1920x1080' "$HOME/Pictures/grim_screenshot_$filename.png"
+grim -t png "$HOME/Pictures/grim_screenshot_$filename.png"
 xclip -selection clipboard -t image/png -i "$HOME/Pictures/grim_screenshot_$filename.png"
