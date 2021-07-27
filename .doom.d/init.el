28cffab95cae15e9424c603d737bb5459b4e28e6
appositum <appositum@protonmail.com>
2021-07-27 11:28:52 -0300

    remove agda and mode packages

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -109,7 +109,7 @@
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
-       agda              ; types of types of types of types...
+       ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;cc                ; C > C++ == 1
        ;;clojure           ; java with a lisp
@@ -130,8 +130,8 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       haskell
        ;;(haskell +dante)  ; a language that's lazier than I am
+       haskell           ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        idris             ; a language you can depend on
        json              ; At least it ain't XML
