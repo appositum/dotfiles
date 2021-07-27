47359e10f60bb039964b5821cd8739dd5538d784
appositum <appositum@protonmail.com>
2021-07-27 10:25:42 -0300

    Delete unsued scripts, rename volume utils folder

--- .config/sway/scripts/volume/mute.pl
+++ .config/sway/scripts/volume/mute.pl
@@ -4,9 +4,9 @@ my $master;
 
 # remove whitespaces
 sub trim {
-	my $s = shift;
-	$s =~ s/^\s+|\s+$//g;
-	return $s;
+    my $s = shift;
+    $s =~ s/^\s+|\s+$//g;
+    return $s;
 };
 
 $master = `amixer sget Master | egrep "%" | head -n1 | awk '{print \$6}'`;
@@ -14,7 +14,7 @@ $master = substr(trim($master), 1, -1);
 printf $master;
 
 if ($master eq "off") {
-  `amixer -q sset Master on`;
+    `amixer -q sset Master on`;
 } else {
-  `amixer -q sset Master off`;
+    `amixer -q sset Master off`;
 }
