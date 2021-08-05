48abaac08cbc47bfdd37edaba567942bc9e65ac2
ezr <appositum@protonmail.com>
2021-08-05 20:08:16 -0300

    Update screenshot scripts

--- .config/sway/scripts/screenshot_full.sh
+++ .config/sway/scripts/screenshot_full.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-filename="$(date "+%F_%H-%M-%S")"
-grim -t png "$HOME/Pictures/grim_screenshot_$filename.png"
-xclip -selection clipboard -t image/png -i "$HOME/Pictures/grim_screenshot_$filename.png"
+filename="grim_screenshot_$(date "+%F_%H-%M-%S").png"
+path="$HOME/Pictures/$filename"
+
+grim -t png $path
+xclip -selection clipboard -t image/png -i $path
+notify-send -t 2000 -a grim "Screenshot saved to clipboard and ~/Pictures/$filename"
