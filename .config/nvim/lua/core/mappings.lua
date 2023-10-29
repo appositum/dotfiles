d752d6f8a0955db03615f95588fb6ffa8e09bce2
appositum <appositum@pm.me>
2023-10-29 19:53:12 -0300

    nvim telescope in normal initial mode for buffers and old files

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -240,10 +240,10 @@ M.telescope = {
     ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
     ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
-    ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
-    ["<leader><Space>"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
+    ["<leader>fb"] = { "<cmd> Telescope buffers initial_mode=normal<CR>", "Find buffers" },
+    ["<leader><Space>"] = { "<cmd> Telescope buffers initial_mode=normal<CR>", "Find buffers" },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
-    ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
+    ["<leader>fo"] = { "<cmd> Telescope oldfiles initial_mode=normal<CR>", "Find oldfiles" },
     ["<leader>fs"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
 
     -- git
