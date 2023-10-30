e1b4a9e152969aa4be597a03e34ee495c1d4b41d
appositum <appositum@pm.me>
2023-10-30 20:23:48 -0300

    nvim highlight current line, dont highlight current context

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -2,10 +2,19 @@
 local M = {}
 
 M.ui = {
-  theme = 'catppuccin',
+  theme = "catppuccin",
   statusline = {
     theme = "default",
     separator_style = "default",
+  },
+  hl_override = {
+    CursorLine = {
+      bg = "lightbg",
+    },
+    CursorLineNr = {
+      bg = "grey",
+      fg = "purple",
+    }
   }
 }
 
