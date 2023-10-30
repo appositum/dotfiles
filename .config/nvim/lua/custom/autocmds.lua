f748f542f2dad663796987ebb44bd618a00272e3
appositum <appositum@pm.me>
2023-10-30 20:24:57 -0300

    nvim add old autocmds

---
+++ .config/nvim/lua/custom/autocmds.lua
@@ -0,0 +1,30 @@
+local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+
+vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
+
+vim.api.nvim_create_autocmd({'InsertEnter'}, {
+  command = 'set number norelativenumber'
+})
+
+vim.api.nvim_create_autocmd({'InsertLeave'}, {
+  command = 'set number relativenumber'
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight on yank',
+  group = group,
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 300})
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'help', 'man'},
+  group = group,
+  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
