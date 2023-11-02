f715abec6b1c408a3823c118858137ec37e387f0
appositum <appositum@pm.me>
2023-11-02 18:57:27 -0300

    nvim custom lsp and mason configs

---
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,26 @@
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+local lspconfig = require("lspconfig")
+local servers = {
+  "html",
+  "cssls",
+  "clangd",
+  "hls",
+  -- "rust-analyzer",
+  "elixirls",
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig["elixirls"].setup {
+  cmd = {
+    "elixir-ls"
+  },
+}
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -56,5 +56,24 @@ return {
         untracked = { text = "┇" },
       }
     }
-  }
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "hls",
+        "rust-analyzer",
+        "elixirls",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
 }
